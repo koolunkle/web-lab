0183073350984efdf23b393a17d6fd01f4acd437
koolunkle <a43361904@gmail.com>
2026-01-02 12:34:58 +0900

    feat: add demo for Tailwind typography and spacing utilities

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg underline font-bold mt-4 text-blue-600">
+        Tailwind makes styling easy!
+      </p>
     </>
   );
 }
