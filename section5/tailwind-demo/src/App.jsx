1c7f5e6013b70090f9e2a2376a1801f1db8df581
koolunkle <a43361904@gmail.com>
2026-01-02 12:39:55 +0900

    feat: add demo for Tailwind background color and border utilities

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
       <p className="text-lg underline font-bold mt-4 text-blue-600">
         Tailwind makes styling easy!
       </p>
+      <div className="bg-gray-200 p-4 m-4">Background color demo</div>
+      <div className="bg-green-300 border border-green-600 p-4 rounded-lg m-4">
+        Border demo
+      </div>
     </>
   );
 }
