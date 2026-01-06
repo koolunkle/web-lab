bd2622036071b0a16d424a831065397a6bdb1f3d
koolunkle <a43361904@gmail.com>
2026-01-06 12:31:02 +0900

    feat: add Theme variables in Tailwind

--- section5/tailwind-demo/src/App.jsx
+++ section5/tailwind-demo/src/App.jsx
@@ -89,6 +89,12 @@ function App() {
       <div className="bg-[#ff5733] h-[165px] w-[300px] border-green-600 p-4 rounded-lg m-4 flex items-center justify-center">
         Custom Width, Height and Color
       </div>
+      <div className="bg-madan-500 h-[165px] w-[300px] border-green-600 p-4 rounded-lg m-4 flex items-center justify-center">
+        Theme variables
+      </div>
+      <div className="bg-primary font-display h-[165px] w-[300px] border-green-600 p-4 rounded-lg m-4 flex items-center justify-center">
+        Font Family and Color variables
+      </div>
     </>
   );
 }
