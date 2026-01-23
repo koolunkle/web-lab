fe567b5dccedec91e44c4f97a968894355be8c02
koolunkle <a43361904@gmail.com>
2026-01-23 12:45:02 +0900

    feat: Eager and Lazy instantiation of Singleton bean

--- section6/example9/src/main/java/com/example/beans/MyService.java
+++ section6/example9/src/main/java/com/example/beans/MyService.java
@@ -1,12 +1,14 @@
 package com.example.beans;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 // @Scope("singleton")
 @Scope(BeanDefinition.SCOPE_SINGLETON) // Optional: Explicitly defining singleton scope
+@Lazy
 public class MyService {
 
     public MyService() {
