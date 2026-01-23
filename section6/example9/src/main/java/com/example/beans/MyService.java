f3b241ed11ab7e28cfad75d4cfabb1f77fed17c8
koolunkle <a43361904@gmail.com>
2026-01-23 12:35:34 +0900

    feat: Singleton Bean scope

---
+++ section6/example9/src/main/java/com/example/beans/MyService.java
@@ -0,0 +1,15 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+// @Scope("singleton")
+@Scope(BeanDefinition.SCOPE_SINGLETON) // Optional: Explicitly defining singleton scope
+public class MyService {
+
+    public MyService() {
+        System.out.println("MyService created");
+    }
+}
