ca17681d13e10d7183dbab73c2c324d81e81bc71
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-27 15:50:18 +0900

    fix: resolve CORS issues by adding CorsFilter configuration

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazystore.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+// @CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     private final IProductService productService;
