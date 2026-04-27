68431cc6688c1a8b03d209a022e288e8b4312882
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-27 17:10:59 +0900

    feat: implement basic event handling in SearchBox

--- section8/easystore-ui/src/components/SearchBox.jsx
+++ section8/easystore-ui/src/components/SearchBox.jsx
@@ -1,4 +1,8 @@
 export default function SearchBox({ label, placeholder, value }) {
+  function handleSearchChange(inputSearch) {
+    console.log(inputSearch);
+  }
+
   return (
     <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
       <label className="text-lg font-semibold text-primary">{label}</label>
@@ -7,6 +11,7 @@ export default function SearchBox({ label, placeholder, value }) {
         className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
         placeholder={placeholder}
         value={value}
+        onChange={() => handleSearchChange("Madan")}
       />
     </div>
   );
