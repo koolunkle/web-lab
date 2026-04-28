a03db0be2ebfa65e7808c76fedf3688e616ebca7
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-28 16:12:17 +0900

    feat: implement client-side routing using react-router-dom

---
+++ section10/easystore-ui/src/components/About.jsx
@@ -0,0 +1,12 @@
+import Footer from "./footer/Footer";
+import Header from "./Header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <div>About</div>
+      <Footer />
+    </>
+  );
+}
---
+++ section10/easystore-ui/src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import About from "./components/About.jsx";
+import Cart from "./components/Cart.jsx";
+import Contact from "./components/Contact.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import "./index.css";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+    {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={appRouter} />
+  </StrictMode>,
+);
