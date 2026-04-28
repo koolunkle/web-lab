498b6cdb0e2e51e3f3860e5ec806b9ad4006e76a
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-28 17:27:51 +0900

    refactor: redefine routes using JSX-based createRoutesFromElements

--- section10/easystore-ui/src/main.jsx
+++ section10/easystore-ui/src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.jsx";
 import About from "./components/About.jsx";
 import Cart from "./components/Cart.jsx";
@@ -10,39 +15,52 @@ import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
 import "./index.css";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/home",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-  },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>,
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
+
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "/home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//       {
+//         path: "/cart",
+//         element: <Cart />,
+//       },
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
