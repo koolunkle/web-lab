fa6fa7853c26200a17926f63c82c528bf605fce8
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-28 22:47:55 +0900

    feat: implement Contacts REST API on the backend

---
+++ section10/eazystore/src/main/java/com/eazybytes/eazystore/entity/Contact.java
@@ -0,0 +1,47 @@
+package com.eazybytes.eazystore.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "mobile_number", nullable = false)
+    private String mobileNumber;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
---
+++ section10/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/ContactServiceImpl.java
@@ -0,0 +1,39 @@
+package com.eazybytes.eazystore.service.impl;
+
+import java.time.Instant;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import com.eazybytes.eazystore.dto.ContactRequestDto;
+import com.eazybytes.eazystore.entity.Contact;
+import com.eazybytes.eazystore.repository.ContactRepository;
+import com.eazybytes.eazystore.service.IContactService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ContactServiceImpl implements IContactService {
+
+    private final ContactRepository contactRepository;
+
+    @Override
+    public boolean saveContact(ContactRequestDto contactRequestDto) {
+        try {
+            Contact contact = transformToEntity(contactRequestDto);
+            contact.setCreatedAt(Instant.now());
+            contact.setCreatedBy(contactRequestDto.getName());
+            contactRepository.save(contact);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    private Contact transformToEntity(ContactRequestDto contactRequestDto) {
+        Contact contact = new Contact();
+        BeanUtils.copyProperties(contactRequestDto, contact);
+        return contact;
+    }
+}
