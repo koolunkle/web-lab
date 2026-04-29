4c103e9f9ec33e0a9430d47f121513c38a3a3437
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-29 21:27:54 +0900

    feat: implement @PathVariable for extracting parameters from URL paths

--- section11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
+++ section11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
@@ -1,8 +1,13 @@
 package com.eazybytes.eazystore.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.eazystore.dto.UserDto;
@@ -15,5 +20,25 @@ public class DummyController {
     public String createUser(@RequestBody UserDto userDto) {
         System.out.println(userDto);
         return "User created successfully";
+    }
+
+    @GetMapping("/search")
+    public String searchUser(@RequestParam(required = false, defaultValue = "Guest", name = "name") String userName) {
+        return "Searching for user : " + userName;
+    }
+
+    @GetMapping("/multiple-search")
+    public String multipleSearch(@RequestParam Map<String, String> params) {
+        return "Searching for user : " + params.get("firstName") + " " + params.get("lastName");
+    }
+
+    // @GetMapping({ "/user/{userId}/posts/{postId}", "/user/{userId}" })
+    // public String getUser(@PathVariable(name = "userId") String id, @PathVariable(required = false) String postId) {
+    //     return "Searching for user : " + id + " and post : " + postId;
+    // }
+
+    @GetMapping({ "/user/map/{userId}/posts/{postId}", "/user/map/{userId}" })
+    public String getUserUsingMap(@PathVariable Map<String, String> pathVariables) {
+        return "Searching for user : " + pathVariables.get("userId") + " and post : " + pathVariables.get("postId");
     }
 }
