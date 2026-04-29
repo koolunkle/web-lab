e44d6a056c1599be55db011f891ef76319d92951
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-04-29 12:16:36 +0900

    feat: pass product data to detail page via location state

--- section10/easystore-ui/src/components/ProductDetail.jsx
+++ section10/easystore-ui/src/components/ProductDetail.jsx
@@ -1,6 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function ProductDetail() {
   const params = useParams();
-  return <div>{params.productId}</div>;
+  const location = useLocation();
+
+  const product = location.state?.product;
+  console.log(product);
+
+  return <div>{product.description}</div>;
 }
