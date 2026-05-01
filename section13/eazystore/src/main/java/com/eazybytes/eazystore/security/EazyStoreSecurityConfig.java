20bc40fcc5cd0230f6c1bb2e2f8e4e4af098dc1f
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-05-01 23:36:21 +0900

    feat: integrate CORS and CSRF configuration into SecurityFilterChain

--- section13/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ section13/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -2,23 +2,67 @@ package com.eazybytes.eazystore.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class EazyStoreSecurityConfig {
+
+    private final List<String> publicPaths;
+
+    // @Bean
+    // SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws
+    // Exception {
+    // return http.authorizeHttpRequests((request) -> request
+    // // .requestMatchers(HttpMethod.GET, "api/v1/products/**").permitAll()
+    // .requestMatchers("api/v1/products/**", "api/v1/contacts/**").permitAll()
+    // .requestMatchers("api/v1/dummy/**").authenticated()
+    // .anyRequest().authenticated())
+    // .formLogin(withDefaults())
+    // .httpBasic(withDefaults())
+    // .build();
+    // }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((request) -> request.requestMatchers("api/v1/products/**").permitAll()
-                .requestMatchers("api/v1/dummy/**").authenticated()
-                .anyRequest().authenticated())
+        return http
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((request) -> {
+                    publicPaths.forEach((path) -> request.requestMatchers(path).permitAll());
+                    request.anyRequest().authenticated();
+                })
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
