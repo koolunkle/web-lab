862fcd80eee1043f8140250f90d1a687d84a276c
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-05-05 17:53:34 +0900

    feat: enable CSRF protection and configure login exclusion

--- section15/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
+++ section15/eazystore/src/main/java/com/eazybytes/eazystore/security/EazyStoreSecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -51,7 +53,9 @@ public class EazyStoreSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 // .csrf(csrfConfig -> csrfConfig.disable())
-                .csrf(csrfConfig -> csrfConfig.ignoringRequestMatchers("/api/v1/auth/login"))
+                // .csrf(csrfConfig -> csrfConfig.ignoringRequestMatchers("/api/v1/auth/login"))
+                .csrf(csrfConfig -> csrfConfig.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
                 .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests((request) -> {
                     publicPaths.forEach((path) -> request.requestMatchers(path).permitAll());
