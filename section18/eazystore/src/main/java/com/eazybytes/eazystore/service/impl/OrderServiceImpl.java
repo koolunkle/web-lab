8ea1332bcba641835a31ab7d044741952bb8ee9d
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-05-07 22:14:56 +0900

    feat: update audit fields manually in custom DML update query

--- section18/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/OrderServiceImpl.java
+++ section18/eazystore/src/main/java/com/eazybytes/eazystore/service/impl/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.eazybytes.eazystore.constants.ApplicationConstants;
@@ -98,7 +100,9 @@ public class OrderServiceImpl implements IOrderService {
 
     @Override
     public void updateOrderStatus(Long orderId, String orderStatus) {
-        orderRepository.updateOrderStatus(orderId, orderStatus);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        orderRepository.updateOrderStatus(orderId, orderStatus, email);
     }
 
     /**
