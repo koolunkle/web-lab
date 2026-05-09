912888135f84425dab0c41f4df2c35624ef95608
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-05-09 11:36:56 +0900

    feat: load external configuration using @PropertySource

--- section20/eazystore/src/main/java/com/eazybytes/eazystore/config/StripeConfig.java
+++ section20/eazystore/src/main/java/com/eazybytes/eazystore/config/StripeConfig.java
@@ -2,12 +2,15 @@ package com.eazybytes.eazystore.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.stripe.Stripe;
 
 import jakarta.annotation.PostConstruct;
 
 @Configuration
+// @PropertySource("file:stripe.properties")
+@PropertySource(value = "classpath:stripe.properties", ignoreResourceNotFound = true)
 public class StripeConfig {
 
     @Value("${stripe.apiKey}")
