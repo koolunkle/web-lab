7b5cdb6b8faa9a19e37aa0ca9ebe5d4c326cd4a1
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-08-09 00:36:11 +0900

    feat(member): 회원 정보 수정 API 구현
    - MemberController 내 내 정보 수정 엔드포인트(PUT /api/info) 추가
    - SecurityContextHolder 기반 인증 사용자 ID 추출 및 MemberDtoRequest id 주입 처리
    - MemberDtoRequest id 프로퍼티 가변(var) 변경
    - MemberService 내 회원 정보 수정 처리 메서드(saveMyInfo) 구현

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -36,4 +36,13 @@ class MemberController(private val memberService: MemberService) {
         val response = memberService.searchMyInfo(userId)
         return BaseResponse(data = response)
     }
+
+    // 내 정보 수정
+    @PutMapping("/info")
+    fun saveMyInfo(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<Unit> {
+        val userId = (SecurityContextHolder.getContext().authentication?.principal as CustomUser).userId
+        memberDtoRequest.id = userId
+        val resultMsg: String = memberService.saveMyInfo(memberDtoRequest)
+        return BaseResponse(message = resultMsg)
+    }
 }
