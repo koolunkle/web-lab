db51984d6ec507c708e173fc3fcf78746969cc61
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-08-09 14:31:03 +0900

    fix(test): DemoApplication 복원, @SpringBootTest classes 명시 지정

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,9 +1,10 @@
 package com.example.demo
 
+import com.example.demo.DemoApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [DemoApplication::class])
 class DemoApplicationTests {
 
 	@Test
