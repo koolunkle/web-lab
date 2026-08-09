cee74b7e491d2e6e0d7f96db2dccf80ad734d25e
KOOLUNKLE\a4336 <a43361904@gmail.com>
2026-08-09 11:58:25 +0900

    refactor(member): MemberRoleRepository 별도 파일로 분리

---
+++ src/main/kotlin/com/example/demo/member/repository/MemberRoleRepository.kt
@@ -0,0 +1,6 @@
+package com.example.demo.member.repository
+
+import com.example.demo.member.entity.MemberRole
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRoleRepository : JpaRepository<MemberRole, Long>
